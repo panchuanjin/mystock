a4e37294df522e714c2907697882d54bc7607dc6
chuanjin.pan <chuanjin.pan@pan.com>
2015-05-24 19:39:18 +0800

    v1

--- pan.py
+++ pan.py
@@ -4,47 +4,30 @@ import urllib.request
 import psycopg2
 import re
 
+for i in range(1,90000):
+    stock_id = format("%06d" %i)
+    print(stock_id)
+    fp = urllib.request.urlopen("http://data.eastmoney.com/report/" +stock_id+".html")
 
+    web_bytes = fp.read()
+    web_str = web_bytes.decode("gbk")
+    fp.close()
 
+    match_number = 0
+    pattern = re.compile(r"强烈推荐")
+    match = pattern.findall(web_str)
+    if match:
+        for result in match:
+            for item in result:
+                match_number = match_number+1
+    else:
+        continue
 
-# python url use:
-fp = urllib.request.urlopen("http://data.eastmoney.com/report/600011.html")
+    # 数据库连接参数
+    conn = psycopg2.connect(database="stock", user="postgres", password="postgres", host="127.0.0.1", port="5432")
+    cur = conn.cursor()
+    cur.execute("INSERT INTO stock(stock_id, raw_data,tuijian)VALUES(%s ,%s, %s)", (stock_id, web_str,match_number))
 
-mybytes = fp.read()
-# note that Python3 does not read the html code as string
-# but as html code bytearray, convert to string with
-mystr = mybytes.decode("gbk")
-
-fp.close()
-
-#print(mystr)
-match_number = 0
-pattern = re.compile(r"强烈推荐")
-match = pattern.findall(mystr)
-if match:
-    for result in match:
-        for item in result:
-            match_number = match_number+1
-#            print(item,end=" ")
-#            print item
-
-print(match_number)
-#print match_number
-
-# 数据库连接参数
-#conn = psycopg2.connect(database="stock", user="postgres", password="postgres", host="127.0.0.1", port="5432")
-#cur = conn.cursor()
-#cur.execute("CREATE TABLE test(id serial PRIMARY KEY, num integer,data varchar);")
-# insert one item
-#cur.execute("INSERT INTO stock(stock_id, stock_name,raw_data)VALUES(%s ,%s, %s)", (1, 'aaa','asdfasf'))
-#cur.execute("INSERT INTO test(num, data)VALUES(%s, %s)", (2, 'bbb'))
-#cur.execute("INSERT INTO test(num, data)VALUES(%s, %s)", (3, 'ccc'))
-
-#cur.execute("SELECT * FROM stock;")
-#rows = cur.fetchall()        # all rows in table
-#print(rows)
-#for i in rows:
-#    print(i)
-#conn.commit()
-#cur.close()
-#conn.close()
+    conn.commit()
+    cur.close()
+    conn.close()
